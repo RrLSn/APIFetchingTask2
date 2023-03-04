7c66721b0f4e07fded2c30a86c5a9364011d9eed
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-03-04 15:43:51 +0100

    Makeup site

--- src/App.jsx
+++ src/App.jsx
@@ -23,16 +23,29 @@ function App() {
     <section className="App bg-[pink] w-[98.7vw] h-[auto] p-[3rem]">
       <h1 className='text-7xl font-[700] text-center text-[#4e4c4c]'>AFOOS MAKEUP</h1>
       <p className='text-center italic underline text-[1.3rem] mt-[.5rem]'>We give the best and Quality</p>
-      <div className='w-[100%] border h-[50rem] border-black mt-[3rem] p-[2rem]'>
-        <h1 className='text-center text-[2.5rem] font-[600]'>Products</h1>
-     </div>
       
-      {/* {makeUps.map((makeup) => (
-        <ProductsCard
-        key={makeup.key} 
-        image={makeup.image_link} />,
-        console.log(makeup)
-      ))} */}
+      <div className='w-[100%] border h-[auto] border-black mt-[3rem] p-[2rem] text-center'>
+        <h1 className=' text-[2.5rem] font-[600]'>Products</h1>
+        
+        {makeUps.length === 0 ? (
+          "Loading..."
+        ) : (
+          <section style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+            {makeUps.map((makeup) => (
+            <ProductsCard
+            key={makeup.id}
+            image={makeup.image_link}
+            brands={makeup.brand}
+            desc={makeup.description}
+            sign={makeup.price_sign}
+            price={makeup.price}
+            cad={makeup.currency}
+            category={makeup.category} />
+          ))}
+          </section>
+        )}
+      </div>
+     
     </section>
   )
 }
--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 const ProductsCard = (props) => {
   return (
-    <div>
-        <img src={props.image} alt="" />
-        <p>{props.name}</p>
-        <p>{desc}</p>
-        <span>
-            <sup>{props.sign}</sup><h1>{props.price}</h1><sub>{props.cad}</sub>
+    <div style={{width: "20rem", minHeight: "15rem",border: "1px solid red", margin: "1rem", padding: "1.5rem"}}>
+        <img className='w-[10rem] h-[13rem] m-auto' src={props.image} alt="" />
+        <p className='mt-[1.5rem] text-[1.3rem] font-[500]'>{props.brands}</p>
+        <p className='mb-[1.5rem] font-[500]'>{props.category}</p>
+        <p>{props.desc}</p>
+        <span className='flex my-[1.5rem]'>
+            <p>{props.sign}</p><h1>{props.price}</h1><p>{props.cad}</p>
         </span>
-        <p></p>
-        <p></p>
-        <button className='w-[3rem] h-[auto] p-[2rem] m-[auto] bg-[#4e4c4c]]'>Buy now</button>
+        <button className='w-[5rem] h-[auto] p-[0.5rem] m-[auto] bg-[red] text-white rounded-md cursor-pointer'>Buy now</button>
     </div>
   )
 }
